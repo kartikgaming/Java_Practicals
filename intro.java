8c7eac2c9b4a9656708e2928e4a48aab4b761df9
Kartik Kurapati <69101177+kartikgaming@users.noreply.github.com>
2024-04-15 11:55:53 +0530

    Add files via upload

---
+++ intro.java
@@ -0,0 +1,9 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("My name is Kartik Kurapati.");
+        System.out.println("My mobile number is 7038535853.");
+        System.out.println("My location is Solapur.");
+        System.out.println("My hobby is playing cricket.");
+        System.out.println("I enjoy problem-solving.");
+    }
+}
